38c1ea464e15390d019907f22a7483d915d072e3
Moonchild <elronnd@elronnd.net>
2019-11-24 11:57:49 -0700

    Fix return: don't have an exception when input is a non-integer; do have one when given more than one arg

--- src/return/return.d
+++ src/return/return.d
@@ -9,16 +9,27 @@
 	Author(s): chaomodus
 */
 
-import std.conv;
 import std.algorithm;
+import std.conv;
+import std.stdio;
 
 int main(string[] args) {
-	int retval = 0;
-	if (args[0].endsWith("false")) {
-		retval = 1;
-	}
-	if (args.length >= 2) {
-		retval = parse!int(args[1]);
-	}
-	return retval;
+  if (args[0].endsWith("false")) {
+    return 1;
+  } else if (args[0].endsWith("true")) {
+    return 0;
+  }
+
+  int retval;
+  if (args.length == 2) {
+    // if return's argument is a non-integer, then it is ignored and 0 returned
+    try {
+      retval = parse!int(args[1]);
+    } catch (Throwable) {}
+  } else if (args.length > 2) {
+    writefln("%s: too many arguments", args[0]);
+    retval = 1;
+  }
+
+  return retval;
 }
