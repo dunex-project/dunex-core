de74f3d5fd1ffe6d8155834b9363d5663671b515
Cas Rusnov <rusnovn@gmail.com>
2020-03-16 21:30:35 -0700

    Multiple updates to ddate
    * Make it more correct
    * Make it support formats.

--- src/ddate/convert.d
+++ src/ddate/convert.d
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2019, DUNEX Contributors
+	Copyright (c) 2019, 2020, DUNEX Contributors
 	Use, modification and distribution are subject to the
 	Boost Software License, Version 1.0.  (See accompanying file
 	COPYING or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -19,15 +19,16 @@ module convert;
 
 import common.ordinal;
 
+import std.array;
 import std.conv;
 import std.datetime.date : Date;
 import std.format;
 import std.math;
-
+import std.stdio;
 
 enum DDayOfWeek : ubyte
 {
-  sm = 0,
+  sm = 1,
   bt,
   pd,
   pp,
@@ -44,8 +45,10 @@ enum DSeason : ubyte
   afm,
 }
 
-static const string[] defaultDayNames = ["Sweetmorn", "Boomtime", "Pungenday", "Prickle-prickle", "Setting Orange"];
-static const string[] defaultSeasonNames = ["Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"];
+static const string[6] defaultDayNames = ["Sweetmorn", "Boomtime", "Pungenday", "Prickle-prickle", "Setting Orange", "St. Tibb's Day"];
+static const string[6] defaultShortDayNames = ["SM", "BT", "PD", "PP", "SO", "ST"];
+static const string[5] defaultSeasonNames = ["Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"];
+static const string[5] defaultShortSeasonNames = ["Chs", "Dsc", "Cfn", "Bcy", "Afm"];
 static const string defaultStTibbsDay = "St. Tibb's Day";
 
 struct DDate {
@@ -69,7 +72,7 @@ public:
       return;
     }
 
-    this.dayOfWeek = cast(DDayOfWeek)((this.dayOfYear % 5) + 1);
+    this.dayOfWeek = cast(DDayOfWeek)(this.dayOfYear % 5);
   }
 
   @safe @nogc this(bool isStTibbs, long year) {
@@ -91,31 +94,116 @@ public:
       this.dayOfWeek = DDayOfWeek.ST;
       return;
     }
-
-    ushort doy = gregorianDate.dayOfYear;
+    auto doy = gregorianDate.dayOfYear - 1;
     if ((gregorianDate.isLeapYear) && (gregorianDate.month > 2)) {
       doy -= 1;
     }
-
-    this.season = cast(DSeason)(doy / 73);
-    this.day = cast(ubyte)(doy - ((this.season - 1) * 73));
-    this.dayOfWeek = cast(DDayOfWeek)((doy - 1 ) % 5);
+    
+    this.season = cast(DSeason)((doy / 73) + 1);
+    this.day = cast(ubyte)((doy - ((this.season - 1) * 73)) + 1);
+    this.dayOfWeek = cast(DDayOfWeek)((doy % 5) + 1);
   }
 
+  /// Return the day of the year.
   @safe @nogc ushort dayOfYear() {
     if (this.isStTibbs)
       return cast(ushort)0;
 
-    return cast(ushort)(((this.season - 1) * 73) + this.day);
+    return cast(ushort)((this.season * 73) + this.day);
   }
 
-  @safe string toString() {
+  /// Support object protocol.
+  @safe string toString() const {
+    return this.toESO1dd1293();
+  }
+
+  
+  /// Convert to ESO 1dd1293 standard format
+  @safe string toESO1dd1293() const {
     if (this.isStTibbs) {
-      return format("%s, YOLD %d", defaultStTibbsDay, this.year);
+      return std.format.format("%s, YOLD %d", defaultStTibbsDay, this.year);
     }
-    return format("%s the %s of %s, YOLD %d", defaultDayNames[cast(int)this.dayOfWeek-1], toOrdinal(this.day), defaultSeasonNames[cast(int)this.season-1], this.year);
+    return this.format("%W the %o of the season of %S, YOLD %y");    
   }
+  
+  /*
+   * Formatting support
+   *
+   * Formats:
+   *   %n - Season number from 1 to 5
+   *   %S - Season name
+   *   %r - short season name
+   *   %w - day of week number from 1 to 5
+   *   %W - day of week name
+   *   %R - short day of week name
+   *   %d - day number of season
+   *   %o - ordinal ("Nth") day number of season
+   *   %y - year number
+   *
+   */
+  @safe string format(string inStr) const {
+    @safe void pad(scope void delegate(string) @safe sink, string padding, uint count) {
+      for (int i = 0; i < count; i++) {
+  	sink(padding);
+      }
+    }
+    @safe void padTo(scope void delegate(string) @safe sink, string padding, uint count, int value) {
+      auto s = to!string(value);
+      uint n = cast(uint)(count - s.length);
+      if ((count != 0) && (n > 0)) {
+  	pad(sink, padding, n);
+      }
+      sink(s);
+    }
+    
+    auto fmt = FormatSpec!char(inStr);
 
+    auto writer = Appender!string();
+    writer.reserve(inStr.length);
+    @safe auto sink = (string x) => writer.put(x);
+    while (fmt.writeUpToNextSpec(writer)) {
+      auto padding  = " ";
+      if (fmt.flZero) {
+	padding = "0";
+      }
+      switch (fmt.spec) {
+      case 'D':
+      case 's':
+	sink(this.toString());
+	break;
+      case 'n':
+	padTo(sink, padding, fmt.width, cast(int)this.season);
+	break;
+      case 'S':
+	sink(defaultSeasonNames[cast(int)this.season - 1]);
+	break;
+      case 'r':
+	sink(defaultShortSeasonNames[cast(int)this.season - 1]);
+	break;
+      case 'w':
+	sink(to!string(cast(uint)this.dayOfWeek));
+	break;
+      case 'W':
+      sink(defaultDayNames[cast(int)this.dayOfWeek-1]);
+      break;
+      case 'R':
+	sink(defaultShortDayNames[cast(int)this.dayOfWeek-1]);
+	break;
+    case 'd':
+      padTo(sink, padding, fmt.width, cast(int)this.day);
+      break;
+      case 'o':
+	sink(toOrdinal(cast(int)this.day));
+	break;
+      case 'y':
+	padTo(sink, padding, fmt.width, cast(int)this.year);
+	break;
+      default:
+	throw new Exception("Unknown format specifier: %" ~ fmt.spec);
+      }      
+    }
+    return to!string(writer.data);
+  }
 }
 
 @safe unittest {
--- src/ddate/ddate.d
+++ src/ddate/ddate.d
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2019, DUNEX Contributors
+	Copyright (c) 2019, 2020, DUNEX Contributors
 	Use, modification and distribution are subject to the
 	Boost Software License, Version 1.0.  (See accompanying file
 	COPYING or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -16,14 +16,53 @@
 
 module app;
 
-
+import common.cmd;
 import convert;
 
 import std.datetime.date : Date;
 import std.datetime.systime: Clock;
-import std.stdio : writeln;
+import std.stdio : stderr, writeln;
+import std.format;
+
+enum APP_NAME = "ddate";
+enum APP_DESC = "convert current date into Discordian date
+
+  Format support:
+  %D - ESO 1dd1293 standard format (default)
+  %n - Season number from 1 to 5
+  %S - Season name
+  %r - short season name
+  %w - day of week number from 1 to 5
+  %W - day of week name
+  %R - short day of week name
+  %d - day number of season
+  %o - ordinal ('Nth') day number of season
+  %y - year number
+
+
+";
+enum APP_VERSION = "1.0 (dunex-core)";
+enum APP_AUTHORS = ["chaomodus"];
+enum APP_LICENSE = import("COPYING");
+enum APP_CAP = ["ddate"];
 
 int main(string[] args) {
-  writeln(DDate(cast(Date)Clock.currTime));
-  return 0;
+  try {
+    return runApplication(args, (Program app) {
+	app.add(new Argument("format", "A format to print out the date in.").optional);
+      },
+      (ProgramArgs args, string[] leftovers) {
+	auto ddate = DDate(cast(Date)Clock.currTime);
+	if (args.arg("format")) {
+	  writeln(ddate.format(args.arg("format")));
+	} else {
+	  writeln(ddate);
+	}
+	return 0;
+      });
+    } catch(Exception ex) {
+    stderr.writeln(APP_NAME, ": ", ex.msg);
+    return 1;
+  }
+
 }
