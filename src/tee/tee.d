0a354c25ad492a59ebe12209f75f0b923170a323
Marie-Joseph <standin@temp.tld>
2020-05-02 16:31:59 -0500

    started work on tee port

--- src/tee/tee.d
+++ src/tee/tee.d
@@ -6,67 +6,70 @@
 
 	tee: Copy stdin to stdout (unbuffered), and, optionally one or more output files.
 	Author(s): chaomodus
-*/
 
+	TODO:
+	* Does not properly write to files, let alone append to them, since port
+*/
+module app;
+
+import common.cmd;
+
+import core.sys.posix.signal;
 import std.exception;
 import std.stdio;
 import std.getopt;
 import std.file;
 import std.process;
-import core.sys.posix.signal;
 
-string output;
-bool ignore_int = false;
-bool append = false;
-bool interrupted = false;
-
-extern (C) void sigint_handler(int signal) nothrow @nogc @system {
-	if (!ignore_int) {
-		interrupted = true;
-	}
-}
+enum APP_NAME = "tee";
+enum APP_DESC = "Copy stdin to stdout, and perhaps some other files.";
+enum APP_VERSION = "1.0 (dunex-core)";
+enum APP_AUTHORS = ["chaomodus"];
+enum APP_LICENSE = import("COPYING");
+enum APP_CAP = [APP_NAME];
 
 int main(string[] args) {
-	auto helpInformation = getopt(args, std.getopt.config.passThrough,
-		"i|ignore-int", "Specify the octal mode of the directory to create.", &ignore_int, 
-		"a|append", "Create the parents of the target directory (if necessary).", &append
-	);
-
-	if (helpInformation.helpWanted) {
-		defaultGetoptPrinter(
-			"Copy stdin to stdout, and perhaps some other files.",
-			helpInformation.options
-		);
-		return 1;
-	}
-
-	signal(SIGINT, &sigint_handler);
-
-	char[1] outbuff;
-	char[] inp;
-	File[] outf = [stdout];
-	if (args.length > 1) {
-		foreach (fname; args[1 .. $]) {
-			outf ~= [File(fname, append ? "a" : "w")];
-		}
-	}
-	while (!interrupted) {
+	return runApplication(args, (Program app) {
+		app.add(new Flag("a", "append", "Append to rather than overwriting output file(s).").name("append").optional);
+		app.add(new Flag("i", "ignore-interrupt", "Ignore the SIGINT signal.").name("ignoreInt").optional);
+		app.add(new Argument("files", "File(s) where output is written.").name("files").optional.repeating);
+	},
+	(ProgramArgs args) {
 		try {
-			inp = stdin.rawRead(outbuff);
-		} catch (ErrnoException e) {
-			writeln("error occured during reading: ", e.msg);
-		}
-		if (inp.length > 0) {
-			foreach (f; outf) {
-				f.rawWrite(inp);
-			}
-		} else if (stdin.eof) {
-			interrupted = true;
-		}
-	}
+			if (args.flag("ignoreInt"))
+				signal(SIGINT, SIG_IGN);
 
-	foreach (f; outf) {
-		f.close();
-	}
-	return 0;
+			char[1] outbuff;
+			char[] inp;
+			File[] outf = [stdout];
+			writeln(args.args("files"));
+			if (args.arg("files").length > 1) {
+				foreach (fname; args.args("files")) {
+					outf ~= [File(fname, args.flag("append") ? "a" : "w")];
+				}
+			}
+			while (true) {
+				try {
+					inp = stdin.rawRead(outbuff);
+				} catch (ErrnoException e) {
+					writeln(APP_NAME, ": error occured during reading: ", e.msg);
+				}
+				if (inp.length > 0) {
+					foreach (f; outf) {
+						f.rawWrite(inp);
+					}
+				} else if (stdin.eof) {
+					break;
+				}
+			}
+
+			foreach (f; outf) {
+				f.close();
+			}
+			return 0;
+		} catch (Exception ex) {
+			stderr.writeln(APP_NAME, ": ", ex.msg);
+			return 1;
+		}
+	});
 }
